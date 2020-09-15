ec71a43944a4255b1f5ea6f09a8291f439b42335
Nisar Hassan Naqvi <syednisarhassan12@gmail.com>
2020-09-15 19:05:46 +0500

    add cypress spec for keyboard shortcuts modal. Fixes #831 (#834)

---
+++ packages/interface/cypress/integration/keyboard-shortcuts-modal.spec.ts
@@ -0,0 +1,14 @@
+/// <reference types="cypress" />
+
+describe('Keyboard shortcuts modal', () => {
+  it('renders the Keyboard shortcuts modal', () => {
+    cy.visit('/');
+    cy.wait(2000);
+    cy.get('input').type('{shift}/');
+    cy.get('.chakra-modal__header').contains('Keyboard Shortcuts');
+  });
+  it('closes the shortcut modal when the overlay is being clicked.', () => {
+    cy.wait(2000);
+    cy.get('.chakra-modal__overlay').click(50, 50);
+  });
+});
