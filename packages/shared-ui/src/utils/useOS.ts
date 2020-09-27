79971c89c97ed47912b2b6158142388f2800ce81
Nisar Hassan Naqvi <syednisarhassan12@gmail.com>
2020-09-27 22:35:11 +0500

    add useOS hook. (#899)
    Fixes #871

---
+++ packages/shared-ui/src/utils/useOS.ts
@@ -0,0 +1,24 @@
+export const useOS = () => {
+  const userAgent = window.navigator.userAgent;
+  const platform = window.navigator.platform;
+  const macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'];
+  const windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'];
+  const iosPlatforms = ['iPhone', 'iPad', 'iPod'];
+  let os;
+
+  if (macosPlatforms.indexOf(platform) !== -1) {
+    os = 'Mac OS';
+  } else if (iosPlatforms.indexOf(platform) !== -1) {
+    os = 'iOS';
+  } else if (windowsPlatforms.indexOf(platform) !== -1) {
+    os = 'Windows';
+  } else if (/Android/.test(userAgent)) {
+    os = 'Android';
+  } else if (!os && /Linux/.test(platform)) {
+    os = 'Linux';
+  } else if (!os) {
+    throw ('Operating System not recognized.');
+  }
+
+  return os;
+};
