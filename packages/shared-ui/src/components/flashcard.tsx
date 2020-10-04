1acbc2487f97bed9e041c4bdf3fc60b0f670a795
nick shook <nick@shook.family>
2020-10-04 13:13:37 -0700

    right forms

--- packages/shared-ui/src/components/flashcard.tsx
+++ packages/shared-ui/src/components/flashcard.tsx
@@ -1,15 +1,22 @@
 /* eslint-disable */
 // @ts-nocheck
 /* eslint-enable */
-import { Box, Button, Text, useColorMode } from '@chakra-ui/core';
-import React, { useRef, useState } from 'react';
+import { Box, Button, Kbd, Text, useColorMode } from '@chakra-ui/core';
+import React, { useEffect, useRef } from 'react';
+import { submitOnMetaEnter, submitOnShiftEnter } from '../utils/keyboard';
 import ReactDiffViewer from 'react-diff-viewer';
 import { Textarea } from '../forms/base';
-import { isShiftEnterPressed } from '../utils/keyboard';
+import { colors } from '../themes/neonLaw';
+import { useForm } from 'react-hook-form';
+import { useIntl } from 'gatsby-plugin-intl';
+import { useLocalStorage } from '../utils/useLocalStorage';
+import { useOS } from '../utils/useOS';
 
 interface FlashcardProps {
   prompt: string;
   answer: string;
+  visible: boolean;
+  key: number;
   showAnswer: boolean;
   toggleShowAnswer: any;
 }
@@ -18,46 +25,86 @@ export const Flashcard = ({
   prompt,
   answer,
   showAnswer,
-  toggleShowAnswer
+  visible,
+  key,
+  toggleShowAnswer,
 }: FlashcardProps) => {
+  const intl = useIntl();
   const { colorMode } = useColorMode();
+  const { handleSubmit, register } = useForm();
+  const OS = useOS();
 
-  const textAreaRef = useRef(null);
+  const localStorageKey = `flashcard-${key}`;
+  const formRef = useRef<HTMLFormElement>(null);
+  const [userAnswer, changeUserAnswer] = useLocalStorage(localStorageKey, '');
 
-  const [userAnswer, changeUserAnswer] = useState('');
+  const onSubmit = ({ userAnswer }) => {
+    changeUserAnswer(userAnswer);
+    toggleShowAnswer(true);
+  };
+
+  const keyDownHandler = (e: any) => {
+    if (OS === 'Mac OS') {
+      submitOnMetaEnter(e, formRef);
+    } else {
+      submitOnShiftEnter(e, formRef);
+    }
+  };
+
+  useEffect(() => {
+    const flashcardDOMSelector = document.querySelector(`.${localStorageKey}`);
+
+    flashcardDOMSelector.addEventListener('keydown', keyDownHandler, false);
+
+    return () => {
+      flashcardDOMSelector.removeEventListener(
+        'keydown',
+        keyDownHandler,
+        false,
+      );
+    };
+  }, [keyDownHandler, localStorageKey]);
 
   return (
-    <Box cursor="pointer" border="1px" borderRadius="0.5em" padding="2em">
+    <Box
+      display={visible ? '!inherit' : 'none'}
+      cursor="pointer"
+      className={localStorageKey}
+      border="1px"
+      borderRadius="0.5em"
+      padding="2em"
+    >
       {!showAnswer ? (
         <>
           <Text className fontSize="1.2em" marginBottom="1em">
             {prompt}
           </Text>
-          <Textarea
-            ref={textAreaRef}
-            onKeyDown={(e: React.KeyboardEvent) => {
-              if (isShiftEnterPressed(e)) {
-                e.preventDefault();
-                toggleShowAnswer(true);
-                setTimeout(() => {
-                  document.querySelector('.show-prompt').focus();
-                }, 10);
-              }
-            }}
-            size="xl"
-            value={userAnswer}
-            onChange={(event) => {
-              changeUserAnswer(event.target.value);
-            }}
-          />
-          <Button
-            marginTop="1em"
-            onClick={() => {
-              toggleShowPrompt(!showPrompt);
-            }}
+          <form
+            onSubmit={handleSubmit(onSubmit as any)}
+            style={{ color: colors.text[colorMode] }}
+            ref={formRef}
           >
-            Show Answer
-          </Button>
+            <Textarea
+              size="xl"
+              name="userAnswer"
+              value={userAnswer}
+              register={register({
+                required: intl.formatMessage({
+                  id: 'forms.answer.required',
+                }),
+              })}
+            />
+            <Button marginTop="1em" type="submit">
+              Show Answer &nbsp;
+              <Kbd border="1px solid #bbb" color="black">
+                CMD
+              </Kbd>
+              &nbsp; + &nbsp;
+              <Kbd border="1px solid #bbb" color="black">
+                Enter
+              </Kbd>
+            </Button>
+          </form>
         </>
       ) : (
         <>
@@ -79,16 +126,9 @@ export const Flashcard = ({
           <Button
             marginTop="1em"
             className="show-prompt"
-            onClick={() => {
-              toggleShowAnswer(false);
-              setTimeout(() => {
-                const text = textAreaRef.current;
-                text.focus();
-                text.setSelectionRange(text.value.length, text.value.length);
-              }, 10);
-            }}
+            onClick={() => toggleShowAnswer(false)}
           >
-              Try Typing the Answer
+            Try Typing the Answer
           </Button>
         </>
       )}
--- packages/shared-ui/src/components/flashcardContainer.tsx
+++ packages/shared-ui/src/components/flashcardContainer.tsx
@@ -3,7 +3,6 @@ import React, { useCallback, useEffect, useState } from 'react';
 import { BsFillCircleFill } from 'react-icons/bs';
 import { Flashcard } from './flashcard';
 import { colors } from '../themes/neonLaw';
-import { isCommandEnterPressed } from '../utils/keyboard';
 import { useAllFlashcardsQuery } from '../utils/api';
 
 const Circle = ({ onClick, key, active }) => {
@@ -23,30 +22,29 @@ export const FlashcardContainer = () => {
   const [selectedFlashcard, changeSelectedFlashcard] = useState(0);
   const [showFlashcardAnswer, changeShowFlashcardAnswer] = useState(false);
 
-  const keyDownListener = useCallback((event) => {
-    // keyCode 37 is the left arrow
-    if (event.keyCode === 37) {
-      if (selectedFlashcard === 0) {
-        return changeSelectedFlashcard(flashcards.length - 1);
+  const keyDownListener = useCallback(
+    (event) => {
+      // keyCode 37 is the left arrow
+      if (event.keyCode === 37) {
+        if (selectedFlashcard === 0) {
+          return changeSelectedFlashcard(flashcards.length - 1);
+        }
+        if (selectedFlashcard > 0) {
+          return changeSelectedFlashcard(selectedFlashcard - 1);
+        }
       }
-      if (selectedFlashcard > 0) {
-        return changeSelectedFlashcard(selectedFlashcard - 1);
+      // keyCode 39 is the right arrow
+      if (event.keyCode === 39) {
+        if (selectedFlashcard === flashcards.length - 1) {
+          return changeSelectedFlashcard(0);
+        }
+        if (selectedFlashcard < flashcards.length - 1) {
+          return changeSelectedFlashcard(selectedFlashcard + 1);
+        }
       }
-    }
-    // keyCode 39 is the right arrow
-    if (event.keyCode === 39) {
-      if (selectedFlashcard === (flashcards.length - 1)) {
-        return changeSelectedFlashcard(0);
-      }
-      if (selectedFlashcard < (flashcards.length - 1)) {
-        return changeSelectedFlashcard(selectedFlashcard + 1);
-      }
-    }
-
-    if (isCommandEnterPressed(event)) {
-      return changeShowFlashcardAnswer(!showFlashcardAnswer);
-    }
-  }, [flashcards, selectedFlashcard, showFlashcardAnswer]);
+    },
+    [flashcards, selectedFlashcard],
+  );
 
   useEffect(() => {
     document.addEventListener('keydown', keyDownListener, false);
@@ -65,14 +63,17 @@ export const FlashcardContainer = () => {
           <Skeleton height="20px" />
         </Stack>
       )}
-      {flashcards.length > 0 && flashcards[selectedFlashcard] && (
-        <Flashcard
-          prompt={flashcards[selectedFlashcard].prompt}
-          answer={flashcards[selectedFlashcard].answer}
-          showAnswer={showFlashcardAnswer}
-          toggleShowAnswer={changeShowFlashcardAnswer}
-        />
-      )}
+      {flashcards.length > 0 &&
+        flashcards.map((flashcard, index) => (
+          <Flashcard
+            key={index}
+            visible={index === selectedFlashcard}
+            prompt={flashcard.prompt}
+            answer={flashcard.answer}
+            showAnswer={showFlashcardAnswer}
+            toggleShowAnswer={changeShowFlashcardAnswer}
+          />
+        ))}
       <Box height="1em" />
       <Flex width="100%">
         <Flex margin="auto">
@@ -81,7 +82,9 @@ export const FlashcardContainer = () => {
               return (
                 <Box margin="0 2px">
                   <Circle
-                    onClick={() => { return; }}
+                    onClick={() => {
+                      return;
+                    }}
                     key={i}
                     active={true}
                   />
@@ -92,7 +95,9 @@ export const FlashcardContainer = () => {
               <Box margin="0 2px" key={i}>
                 <Circle
                   key={i}
-                  onClick={() => { changeSelectedFlashcard(i); }}
+                  onClick={() => {
+                    changeSelectedFlashcard(i);
+                  }}
                   active={false}
                 />
               </Box>
@@ -102,31 +107,23 @@ export const FlashcardContainer = () => {
       </Flex>
       <Box marginTop="1em">
         <Text>
-          Press
-          &nbsp;
+          Press &nbsp;
           <Kbd border="1px solid #bbb" color="black">
             Left
           </Kbd>
-          &nbsp;
-          or
-          &nbsp;
+          &nbsp; or &nbsp;
           <Kbd border="1px solid #bbb" color="black">
             Right
           </Kbd>
-          &nbsp;
-          to navigate among the flashcards. Press
-          &nbsp;
+          &nbsp; to navigate among the flashcards. Press &nbsp;
           <Kbd border="1px solid #bbb" color="black">
             CMD
           </Kbd>
-          &nbsp;
-          +
-          &nbsp;
+          &nbsp; + &nbsp;
           <Kbd border="1px solid #bbb" color="black">
             Enter
           </Kbd>
-          &nbsp;
-          to toggle the answer.
+          &nbsp; to toggle the answer.
         </Text>
       </Box>
     </Box>
