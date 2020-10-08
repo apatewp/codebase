f6b5931ae664b8845bc6a6bf54e14ce16c41c19b
nick shook <nick@shook.family>
2020-10-08 11:37:48 -0700

    try 2 with logrocket *NO PR*

--- packages/interface/gatsby-browser.js
+++ packages/interface/gatsby-browser.js
@@ -1,13 +1,6 @@
 import { BaseLayout } from '@neonlaw/shared-ui/src/layouts/base';
-import LogRocket from 'logrocket';
 import React from 'react';
 
 export const wrapPageElement = ({ element, props }) => {
   return <BaseLayout {...props}>{element}</BaseLayout>;
-};
-
-export const onClientEntry = () => {
-  if (process.env.GATSBY_LOGROCKET_CREDENTIALS) {
-    LogRocket.init(process.env.GATSBY_LOGROCKET_CREDENTIALS);
-  }
 };
--- packages/interface/gatsby-ssr.js
+++ packages/interface/gatsby-ssr.js
@@ -6,34 +6,59 @@ export const wrapPageElement = ({ element, props }) => {
   return <BaseLayout {...props}>{element}</BaseLayout>;
 };
 
-export const onPreRenderHTML = (
-  { getHeadComponents, replaceHeadComponents }
-) => {
+export const onPreRenderHTML = ({
+  getHeadComponents,
+  replaceHeadComponents,
+}) => {
   const headComponents = getHeadComponents();
 
   headComponents.push(
-    <script key="ze-settings" src="/scripts/ze-settings.js" />
+    <script key="ze-settings" src="/scripts/ze-settings.js" />,
   );
 
   replaceHeadComponents(headComponents);
 };
 
-export const onRenderBody = ({ 
-  setPostBodyComponents, 
-  setPreBodyComponents 
+export const onRenderBody = ({
+  setPostBodyComponents,
+  setPreBodyComponents,
 }) => {
   setPreBodyComponents([<ColorModeScript key="chakra-ui-no-flash" />]);
   if (
     process.env.NODE_ENV == 'development' ||
-    process.env.GATSBY_ACTIVE_ENV == 'development' ||
-    process.env.GATSBY_ACTIVE_ENV == 'staging'
+    process.env.GATSBY_ACTIVE_ENV == 'development'
   ) {
+    return;
+  }
+  if (process.env.GATSBY_ACTIVE_ENV == 'staging') {
+    setPostBodyComponents([
+      <script
+        src="https://cdn.lr-ingest.io/LogRocket.min.js"
+        key="logrocket-cdn"
+        crossOrigin="anonymous"
+      />,
+      /* eslint-disable */
+      <script key="logrocket-init">
+        window.LogRocket && window.LogRocket.init('4qbrpw/staging');
+      </script>,
+      /* eslint-enable */
+    ]);
     return;
   }
 
   const zendeskKey = '81e26970-baa7-4b83-a913-984711a0b5f1';
 
   setPostBodyComponents([
+    <script
+      src="https://cdn.lr-ingest.io/LogRocket.min.js"
+      key="logrocket-cdn"
+      crossOrigin="anonymous"
+    />,
+    /* eslint-disable */
+    <script key="logrocket-init">
+      window.LogRocket && window.LogRocket.init('4qbrpw/production-hoje7');
+    </script>,
+    /* eslint-enable */
     <script
       id="ze-snippet"
       key="zendesk"
@@ -45,6 +70,6 @@ export const onRenderBody = ({
       site="DUBLGHDJ"
       excluded-domains="127.0.0.1,localhost,neonlaw.net"
       defer
-    />
+    />,
   ]);
 };
