87ad79fc51b15e664202b44722dbb33f28b782f9
Nisar Hassan Naqvi <syednisarhassan12@gmail.com>
2020-11-06 22:00:23 +0500

    adjust Date Picker styles. (#1067)
    Fixes #924
    
    Co-authored-by: nick shook <nick@shook.family>

--- packages/shared-ui/src/components/questions/singleDateQuestion.tsx
+++ packages/shared-ui/src/components/questions/singleDateQuestion.tsx
@@ -1,4 +1,4 @@
-import { Box, Heading } from '@chakra-ui/core';
+import { Heading, useColorMode } from '@chakra-ui/core';
 import React, { useState } from 'react';
 
 import DatePicker from 'react-datepicker';
@@ -6,6 +6,7 @@ import { FlashButton } from '../button';
 import { Section } from '../section';
 import { gutters } from '../../themes/neonLaw';
 import { navigate } from 'gatsby-plugin-intl';
+import styled from '@emotion/styled';
 import { useIntl } from 'gatsby-plugin-intl';
 
 interface SingleDateInterface {
@@ -15,6 +16,26 @@ interface SingleDateInterface {
   questionnairePath: string;
   updateAnswers: any;
 }
+
+const StyledDateInput = styled.button`
+  border: 1px solid;
+  padding: .3rem ${gutters.xSmallOne};
+`;
+
+const DateInput = ({value, onClick}) => {
+  const {colorMode} = useColorMode();
+
+  return (
+    <StyledDateInput 
+      onClick={onClick}
+      style={{
+        borderColor: colorMode === 'light' ? '#bbb' : '#999'
+      }}
+    >
+      {value}
+    </StyledDateInput>
+  );
+};
 
 export const SingleDateQuestion = ({
   id,
@@ -39,28 +60,30 @@ export const SingleDateQuestion = ({
   const intl = useIntl();
 
   return (
-    <Box>
-      <Section>
-        <Heading as="h3" fontWeight="normal">
-          {prompt}
-        </Heading>
-        <DatePicker
-          selected={chosenDate}
-          onChange={(date) => setChosenDate(date)}
-        />
-        <br />
-        <FlashButton
-          buttonScheme="teal"
-          containerStyles={{margin: `${gutters.xSmallOne} 0`}}
-          onClick={() => {
-            updateAnswers(id, chosenDate);
+    <Section>
+      <Heading as="h3" fontWeight="normal">
+        {prompt}
+      </Heading>
+      <DatePicker
+        calendarClassName="calendar"
+        selected={chosenDate}
+        onChange={(date) => setChosenDate(date)}
+        // eslint-disable-next-line
+        // @ts-ignore
+        customInput={<DateInput />}
+      />
+      <br />
+      <FlashButton
+        buttonScheme="teal"
+        containerStyles={{margin: `${gutters.xSmallOne} 0`}}
+        onClick={() => {
+          updateAnswers(id, chosenDate);
 
-            navigate(nextStepPath());
-          }}
-        >
-          {intl.formatMessage({ id: 'components_questions.submit' })}
-        </FlashButton>
-      </Section>
-    </Box>
+          navigate(nextStepPath());
+        }}
+      >
+        {intl.formatMessage({ id: 'components_questions.submit' })}
+      </FlashButton>
+    </Section>
   );
 };
